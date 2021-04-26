80c7b080408d82413e45fad4059a9ce4f84f9e16
Juan David Robles <robles@experimentality.co>
2021-04-25 22:43:45 -0500

    Mercadopago test development

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var exphbs  = require('express-handlebars');
-var port = process.env.PORT || 3000
+var fs = require('fs');
+var port = process.env.PORT || 3000;
 
 var app = express();
  
@@ -15,8 +16,26 @@ app.get('/', function (req, res) {
     res.render('home');
 });
 
+app.get('/success', function (req, res) {
+    res.render('success', req.query);
+});
+
+app.get('/failure', function (req, res) {
+    res.render('failure', req.query);
+});
+
+app.get('/pending', function (req, res) {
+    res.render('pending', req.query);
+});
+
 app.get('/detail', function (req, res) {
     res.render('detail', req.query);
+});
+
+app.post('/notifications', (req, res) => {
+    const datetime = new Date();
+    fs.writeFileSync('./data/notifications/' + datetime.toISOString() + '.json', JSON.stringify(req.body || 'Body vacío'));
+    res.status(201);
 });
 
 app.listen(port);
---
+++ data/notifications/readme.txt
@@ -0,0 +1,1 @@
+Esta carpeta contiene todos los archivos de notificaciones creadas
